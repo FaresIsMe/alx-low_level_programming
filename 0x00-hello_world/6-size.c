333c75d18aa4abffe5e32fd87af47e6d402f7aac
Your Name <you@example.com>
2023-07-13 09:11:39 -0700

    that is bull***

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main -entry
+ *
+ * Description: it also prints
+ *
+ * Return: Always 0 (Success)
+*/
+
+
+
+int main(void)
+{
+printf("Size of char: %d byte(s)", sizeof(char));
+printf("Size of int: %d bytes(s)", sizeof(int));
+printf("Size of long int: %d bytes(s)", sizeof(long int));
+printf("Size of long long int: %d bytes(s)", sizeof(long long int));
+printf("Size of float: %d bytes(s)", sizeof(float));
+return (0);
+}
