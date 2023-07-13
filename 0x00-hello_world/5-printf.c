3c475767e52c4622d556460b014e8f9e0cdddf63
Your Name <you@example.com>
2023-07-13 08:20:53 -0700

    hope

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main -Entry
+ *
+ * desriprion: a c that prints
+ *
+ * Return Always 0 (success)
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
