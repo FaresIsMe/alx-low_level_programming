57dd66b18c70a27f2f808e2c9854bbfbfab8409f
Fares Ahmed <fareselzeny6@gmail.com>
2023-07-14 19:12:16 +0300

    0-9 again

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main-Entyy
+ *Decription: idk
+ *Return: Always 0
+*/
+int main(void)
+{
+	char i = '0';
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
