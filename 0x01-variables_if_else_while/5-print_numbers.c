03a1f663a893d08901c15c3da9ef56c2e0c2311e
Fares Ahmed <fareselzeny6@gmail.com>
2023-07-14 18:15:06 +0300

    10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main-Entyy
+ *Decription: idk
+ *Return: Always 0
+*/
+int main(void)
+{
+	int i = 0;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	return (0);
+}
