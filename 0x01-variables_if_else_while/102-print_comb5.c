1e1422aaf370ca996f9fa362c40dcebce8a06f68
FaresIsMe <125498341+FaresIsMe@users.noreply.github.com>
2023-07-15 09:18:30 +0300

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -17,22 +17,25 @@ int i, j, k, l;
 
 for (i = 0; i <= 9; i++)
 {
-	for (j = 0; j <= 9; j++)
+for (j = 0; j <= 9; j++)
+for (k = i; k <= 9; k++)
 {
-	for (k = i; k <= 9; k++)
-	{
-		for (l = j + 1; l <= 9; l++)
-		{
-			int l_start = (k == i) ? j + 1 : 0;
+if (k == i)
+{
+l = j + 1;
+}
+else
+{
+l = 0;
+}
+for (; l <= 9; l++)
+{
+putchar(i + '0');
+putchar(j + '0');
+putchar(' ');
+putchar(l + '0');
 
-			for (l = l_start; l <= 9; l++)
-			{
-				putchar(i + '0');
-				putchar(j + '0');
-				putchar(' ');
-				putchar(k + '0');
-				putchar(l + '0');
-if (i != 9 || j != 8 || k != 9 || l != 9)
+if (!(i == 9 && j == 8 && k == 9 && l == 9))
 {
 putchar(',');
 putchar(' ');
@@ -41,7 +44,6 @@ putchar(' ');
 }
 }
 }
-}
-putchar('\n');
 return (0);
 }
+
