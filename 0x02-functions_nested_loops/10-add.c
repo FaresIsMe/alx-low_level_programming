027ad2a917faa94833d333387894b32fb12a930c
Fares Ahmed <fareselzeny6@gmail.com>
2023-07-18 21:41:49 +0300

    hihhhihihih

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *add -checks
+ *@n: the c
+ *@b: the b
+ *Description: nothing
+ *Return: 1 if c lower, 0 if not
+*/
+int add(int n, int b)
+{
+	int k = b + b
+		;
+	return (k);
+}
