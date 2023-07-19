3ea028cf556729b811f75c94d623007988a9c8ed
Fares Ahmed <fareselzeny6@gmail.com>
2023-07-19 17:12:36 +0300

    hi

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,13 @@
 #include "main.h"
 /**
- *main - test
- *
- *Return :0
+ *main - test is test
+ *Return: 0
 */
 int main(void)
 {
 	int i;
-	
-	i = 98;
-	if (positive_or_negative(i) == 0)
-		printf("0 is zero");
-	return (o);
-}
 
+	i = 98 % 2;
+	positive_or_negative(i);
+	return (0);
+}
