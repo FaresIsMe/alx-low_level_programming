db47d41569c7bba48e462bb643d9ed91886dc613
Fares Ahmed <fareselzeny6@gmail.com>
2023-07-20 16:29:10 +0300

    0 to 14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *more_numbers - from 0 to 14 ten times!!
+ *Return: nothing
+*/
+
+void more_numbers(void)
+{
+	int i;
+	int k;
+
+	for (k = 0; k < 10; k++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
