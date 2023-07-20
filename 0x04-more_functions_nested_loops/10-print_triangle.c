b1d1429501798b77d82ea716aef291be2b424669
Fares Ahmed <fareselzeny6@gmail.com>
2023-07-20 21:14:32 +0300

    tri

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ *print_triangle - makes a Ttiangle with base Size
+ *@size: The base length
+ *Return: nothing
+*/
+void print_triangle(int size)
+{
+if (size > 0)
+{
+	int i, j, k;
+
+	for (i = 0; i < size; i++)
+	{
+		for (k = size; k > i; k--)
+		{
+			_putchar(' ');
+		}
+		for (j = 0; j <= i; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
+else
+{
+_putchar('\n');
+}
+}
