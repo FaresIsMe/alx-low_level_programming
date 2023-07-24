6aa9a641f6e7f6532e70b60302ba4ba18f835bbc
Fares Ahmed <fareselzeny6@gmail.com>
2023-07-24 19:32:43 +0300

    hi

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ *rev_string - some pointer
+ *@s: some pointer
+ * Return: the length of the string s
+*/
+void rev_string(char *s)
+{
+	strrev(s);
+}
