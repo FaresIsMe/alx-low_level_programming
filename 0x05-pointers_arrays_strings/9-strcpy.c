ae1ec9012ae04c780615880ae1a2652bbc28a67e
Fares Ahmed <fareselzeny6@gmail.com>
2023-07-24 22:18:14 +0300

    ahahah

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ *_strcpy - it copies strings 
+ *@dest: the copy
+ *@src: the original string
+ *Retuen: string copied
+*/
+char *_strcpy(char *dest, char *src)
+{
+		int i;
+		int L;
+
+		L = strlen(src);
+		for (i = 0; i <= L ; i++)
+		{
+				dest[i] = src[i];
+		}
+				return(src);
+}
