a5ebc05f6a9afe461b5f5fe796ad46b795b15ace
Fares Ahmed <fareselzeny6@gmail.com>
2023-07-29 13:20:23 +0300

    hiya

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,38 +2,30 @@
 #include <string.h>
 /**
  * leet - capitalizes all words of a string
- * @s: the string to capitalize
+ * @str: the string to capitalize
  *
  * Return: a pointer to the modified string
  */
-char *leet(char *s)
+char *leet(char *str)
 {
-	int i;
-	int L;
+		char *ptr = str;
+		char *leetChars = "aAeEoOtTlL";
+		char *leetNums = "4433007711";
 
-	L = strlen(s);
-	for (i = 0; i < L; i++)
-	{
-		if (s[i] == 'a' || s[i] == 'A')
+		while (*ptr)
 		{
-			s[i] = '4';
+			int i = 0;
+
+			while (leetChars[i])
+			{
+				if (*ptr == leetChars[i])
+					{
+					*ptr = leetNums[i];
+break;
+					}
+						i++;
+				}
+				ptr++;
 		}
-		else if (s[i] == 'e' || s[i] == 'E')
-		{
-			s[i] = '3';
-		}
-		else if (s[i] == 'o' || s[i] == 'O')
-		{
-			s[i] = '0';
-		}
-		else if (s[i] == 't' || s[i] == 'T')
-		{
-			s[i] = '7';
-		}
-		else if (s[i] == 'l' || s[i] == 'L')
-		{
-			s[i] = '1';
-		}
-	}
-	return (s);
+		return (str);
 }
