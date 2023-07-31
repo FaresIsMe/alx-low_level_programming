76198ed386d068701f9d9ac5eafa41153c494027
Fares Ahmed <fareselzeny6@gmail.com>
2023-07-31 13:59:00 +0300

    hi

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ *_memset - int fills a memory block
+ *@s: idk
+ *@b: still dk
+ *@n: dk
+ *Return: anything
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+	return (memset(s, b, n));
+}
+
