a7e848849c4524d47e0c80a9d848fa1d93f68e18
Fares Ahmed <fareselzeny6@gmail.com>
2023-07-31 17:18:46 +0300

    hiyyyyya

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+/**
+ *_strstr - int fills a memory block
+ *@haystack: idk
+ *@needle: still dk
+ *Return:dest
+*/
+char *_strstr(char *haystack, char *needle)
+{
+	return (strstr(haystack, needle));
+}
