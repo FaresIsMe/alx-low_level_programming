fc650fe288d8a45ffbcdc90b7d2605d2942e1a11
Fares Ahmed <fareselzeny6@gmail.com>
2023-07-31 16:00:23 +0300

    idk what is that

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+/**
+ *_strspn - int fills a memory block
+ *@s: idk
+ *@accept: still dk
+ *Return:dest
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+	return (strspn(s, accept));
+}
