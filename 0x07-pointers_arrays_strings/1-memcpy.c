0567716dc6151162907d4458f9d29dbae6cb75da
Fares Ahmed <fareselzeny6@gmail.com>
2023-07-31 14:38:12 +0300

    hiya

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ *_memcpy - int fills a memory block
+ *@dest: idk
+ *@src: still dk
+ *@n: dk
+ *Return:dest
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	return (memcpy(dest, src, n));
+}
