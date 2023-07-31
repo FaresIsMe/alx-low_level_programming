a33d1aca2f63d055bf213c68e32cfa717c6f0b3f
Fares Ahmed <fareselzeny6@gmail.com>
2023-07-31 16:53:40 +0300

    not in the mood

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+/**
+ *_strpbrk - int fills a memory block
+ *@s: idk
+ *@accept: still dk
+ *Return:dest
+*/
+char *_strpbrk(char *s, char *accept)
+{
+	return (strpbrk(s, accept));
+}
