1a09c153bdd92da928a19d49020112b983ac0e78
Fares Ahmed <fareselzeny6@gmail.com>
2023-08-04 23:21:24 +0300

    hiiyyya

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *main - entry
+ *Description: IDK what it do more than this but cooool
+ *@argv: idk
+ *@argc: idk
+ *Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	argv[0] = argv[2 - 2];
+	return (0);
+}
