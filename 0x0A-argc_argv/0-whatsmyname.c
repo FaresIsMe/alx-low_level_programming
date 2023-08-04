2d54dcecf698f9f51afb9541d6cfe8f9633716fc
Fares Ahmed <fareselzeny6@gmail.com>
2023-08-04 23:13:03 +0300

    hiiiya

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,12 +2,13 @@
 #include "main.h"
 
 /**
- *main _entry
+ *main - entry
+ *Description: IDK what it do more than this but cooool
  *@argv: idk
  *@argc: idk
  *Return: 0
 */
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[argc - argc]);
 	return (0);
