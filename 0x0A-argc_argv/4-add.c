3f0c7c2de25f5b5ebe4cd3d675d04bdb0baa602b
Fares Ahmed <fareselzeny6@gmail.com>
2023-08-05 00:21:39 +0300

    hiyyya

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *main - entry
+ *Description: IDK what it do more than this but cooool
+ *@argv: idk
+ *@argc: idk
+ *Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	int i;
+	int sum = 0;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (*argv[i] >= 48 && *argv[i] <= 57)
+				sum = sum + atoi(argv[i]);
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		printf("%d\n", sum);
+		return (0);
+	}
+	else
+		printf("%d\n", 0);
+	return (0);
+}
