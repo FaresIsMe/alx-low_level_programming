ab7a383cd41b1ca9c5cb2de8730ac77e367813f2
Fares Ahmed <fareselzeny6@gmail.com>
2023-08-07 21:40:17 +0300

    hiiiii

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * alloc_grid - creates a dimentional array in the heap
+ * @width: IDK
+ * @height: whatever man it's height
+ * Return: a double pointer to the array, or NULL if it fails
+ */
+int **alloc_grid(int width, int height)
+{
+	int **ptr;
+	int i;
+	int j;
+
+	if (width <= 0 || height <= 0)
+	return (NULL);
+
+	ptr = malloc(sizeof(int) * height);
+
+	for (i = 0; i < height; i++)
+	{
+		ptr[i] = malloc(sizeof(int) * width);
+		for (j = 0; j < width; j++)
+		{
+			ptr[i][j] = 0;
+		}
+	}
+	return (ptr);
+}
+
