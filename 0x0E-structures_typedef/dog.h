c5c1bd8fb6170e6e6019cb052e5b5de538cca520
Fares Ahmed <fareselzeny6@gmail.com>
2023-08-14 09:02:47 +0300

    hahah

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ *struct dog - a structure for dogs
+ *@name: it's a name
+ *@age: some float
+ *@owner: idk
+ *
+ *Description: idk
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
