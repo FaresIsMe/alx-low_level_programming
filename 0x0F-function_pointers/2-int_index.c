0672ade90d9ce55737576f8186ce46a117f09acb
Fares Ahmed <fareselzeny6@gmail.com>
2023-08-16 18:53:58 +0300

    hallo

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,9 +13,9 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (size <= 0 || cmp == NULL || array == NULL)
 	return (-1);
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i <= size; i++)
 	{
-		if (i == size - 1)
+		if (i == size)
 		return (-1);
 		if (cmp(array[i]))
 			return (i);
