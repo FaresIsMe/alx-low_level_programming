fb932bbe223aa442d887aed8712d5d0af259a4db
Fares Ahmed <fareselzeny6@gmail.com>
2023-08-16 18:49:06 +0300

    hallo

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+/**
+ * int_index - wrappeer function
+ * @array: the name to ba passed
+ * @size: the size of the array
+ * @cmp: the function that name ti be passed to
+ * Return: Nothing
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0 || cmp == NULL || array == NULL)
+	return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (i == size - 1)
+		return (-1);
+		if (cmp(array[i]))
+			return (i);
+	}
+	return (-1);
+}
