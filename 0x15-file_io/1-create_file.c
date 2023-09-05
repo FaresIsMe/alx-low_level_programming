7191e27e5949ee915eb9db4a561d8cb34758b1a6
Fares Ahmed <fareselzeny6@gmail.com>
2023-09-05 21:05:20 +0300

    IDK

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,6 +1,5 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 /**
  * create_file - creats a file
  * @filename: the name of the file
@@ -9,22 +8,28 @@
 */
 int create_file(const char *filename, char *text_content)
 {
-	FILE *fp;
+	int fd;
+	ssize_t sz, len;
 
 	if (filename == NULL)
 	return (-1);
 
-	fp = fopen(filename, "rw");
-	if (fp == NULL)
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+
+	if (fd == -1)
 	return (-1);
-	else
+
+	if (text_content)
 	{
-		if (fputs(text_content, fp) == EOF || ferror(fp))
+		len = strlen(text_content);
+		sz = write(fd, text_content, len);
+
+		if (sz != len)
 		{
-			fclose(fp);
+			close(fd);
 			return (-1);
 		}
-		fclose(fp);
-		return (1);
 	}
+		close(fd);
+		return (1);
 }
