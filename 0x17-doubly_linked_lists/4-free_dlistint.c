07aed5365d14f94e72e5f6e0214721f9d0326b05
Fares Ahmed <fareselzeny6@gmail.com>
2023-10-13 06:50:24 +0300

    [Add] -4

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -2,14 +2,14 @@
 /**
  * free_dlistint - it frees the list
  * @head: pointer the node's head
- * 
+ *
  * Return: nothing
 */
 void free_dlistint(dlistint_t *head)
 {
-    while(head)
-    {
-        free(head);
-        head = head->next;
-    }
+	while (head)
+	{
+		free(head);
+		head = head->next;
+	}
 }
