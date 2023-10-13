8e373d5f9c48a07700f0de1a263dda9e119cc965
Fares Ahmed <fareselzeny6@gmail.com>
2023-10-13 09:52:48 +0300

    [Add] -7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -15,7 +15,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 0)
 	{
 		new_node = add_dnodeint(*&h, n);
-		return (new_node);
+		return (*h);
 	}
 
 	new_node = malloc(sizeof(dlistint_t));
