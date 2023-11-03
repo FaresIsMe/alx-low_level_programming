b6e7198236c513704ca1002cccac452e9b38805d
Fares Ahmed <fareselzeny6@gmail.com>
2023-11-03 18:20:40 +0200

    Add to the table

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,54 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - 
+ * @key: is the key
+ * @ht:  is the hash table
+ * @value: is the value associated with the key. 
+ * 
+ * Returns: 1 if it succeeded, 0 otherwise
+*/
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+    unsigned long int index;
+    hash_node_t *new_node, *head;
+
+    if (ht == NULL || key == NULL || *key == '\0')
+        return 0;
+
+    index = key_index((const unsigned char *)key, ht->size);
+    head = ht->array[index];
+
+
+    while (head != NULL)
+    {
+        if (strcmp(head->key, key) == 0)
+        {
+            /* Key already exists, update the value */
+            free(head->value);
+            head->value = strdup(value);
+            if (head->value == NULL)
+                return 0;
+            return 1;
+        }
+        head = head->next;
+    }
+
+    new_node = malloc(sizeof(hash_node_t));
+    if (new_node == NULL)
+        return 0;
+    new_node->key = strdup(key);
+    new_node->value = strdup(value);
+    if (new_node->key == NULL || new_node->value == NULL)
+    {
+        free(new_node->key);
+        free(new_node->value);
+        free(new_node);
+        return 0;
+    }
+    new_node->next = ht->array[index];
+    ht->array[index] = new_node;
+
+    return 1;
+}
